1d0147b4e34adbc3c858a55cfc9a6d16b83a0ea1
mokshanirgutti <mokshasai910@gmail.com>
2024-10-12 20:16:05 +0530

    changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -68,25 +68,32 @@ const Section1 = () => {
   const circle = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
 
   return (
-    <div className="h-[200vh] bg-green-500 text-[3.5vw] text-white pb-[10vh] sticky top-0 -z-10 2xl:bg-red-500 overflow-hidden">
+    <div className="lg:h-[200vh] bg-green-500 text-[3.5vw] text-white pb-[10vh] sticky top-0 -z-10 xl:bg-slate-500 2xl:bg-neutral-600 overflow-hidden">
       {/* Moving heading */}
-      <motion.div style={{ y: heading }} className="absolute left-32 top-[35vh]">
+      <motion.div style={{ y: heading }} className="absolute left-32 top-[35vh] hidden lg:block">
         <h1 className="text-3xl">Lorem ipsum dolor sit amet.</h1>
       </motion.div>
 
+
       {/* Red div moving to the left */}
-      <motion.div style={{ x: divMove }} className="bg-red-400 h-[80vh] w-[70vw] absolute -right-[36vw] top-24 2xl:top-[25vh] z-20 "></motion.div>
+      <motion.div style={{ x: divMove }} className="bg-red-400 h-[80vh] w-[70vw] absolute -right-[36%] top-32  z-20 hidden lg:block "></motion.div>
 
       {/* Yellow circle fading out */}
       <motion.div
         style={{ opacity: circle }}
-        className="bg-yellow-400 h-[13vw] w-[13vw] absolute right-96 top-56 rounded-full 2xl:top-[40vh] z-30 "
+        className="bg-yellow-400 h-44 w-44 absolute lg:right-[28%] lg:top-[16.7%] xl:top-[14.7%] xl:right-[28.2%] 2xl:right-[30.2%] 2xl:top-[17.3%] rounded-full  z-30 hidden lg:block "
       ></motion.div>
       <Ripple
-            className="absolute -top-[105vh] -right-[30vw] z-10"
+            className="absolute -top-[57%] -right-[30vw] z-10 hidden lg:block"
             numCircles={3}
             mainCircleSize={200}
           />
+
+          <div className='mx-7 py-32 flex flex-col gap-6 lg:hidden md:mx-auto max-w-md'>
+          <h1 className="text-3xl font-semibold">Lorem ipsum dolor sit amet.</h1>
+          <p className='text-xl font-normal'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, magni.</p>
+          <div className='bg-red-300 h-44'></div>
+          </div>
     </div>
   );
 };
@@ -95,7 +102,7 @@ const Section1 = () => {
 
 const Section2 = () => {
   return (
-    <div className="h-[200vh]  bg-white  pb-[10vh] ">
+    <div className="h-screen lg:h-[200vh]  bg-white  pb-[10vh] ">
       <OtherSection/>
     </div>
   );
